73b380a1d9e0a8557fa56724b1498355f874e303
AnkitKumar8080 <ankitkumar44.80.80@gmail.com>
2025-01-10 21:50:16 +0530

    added vercel analytics

--- frontend/src/app/(main)/page.tsx
+++ frontend/src/app/(main)/page.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import CanvasWrapper from "../../components/canvas/CanvasWrapper";
+import { Analytics } from "@vercel/analytics/react";
 
 const Home = () => {
   return (
     <div>
+      <Analytics />
       <CanvasWrapper />
     </div>
   );
